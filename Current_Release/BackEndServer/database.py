b2e63a5b49e10a6dcff9d584824e2fc29346d0d4
Brock Heinz <63028258+BrockHeinz@users.noreply.github.com>
2023-04-28 11:13:23 -0700

    Updated database.py for use of connection.py and typelock.sql

--- Current_Release/BackEndServer/database.py
+++ Current_Release/BackEndServer/database.py
@@ -1,77 +1,64 @@
+import connection
 import mysql.connector
 
-class Connection:
-    # Connection object constructor
-    def __init__(self, host="localhost", user="root", pwd="", db=""):
-        self.__myDB = None
-        self.__host = host
-        self.__user = user
-        self.__pwd = pwd
-        self.__db = db
+# name: setItUp
+# parameters: none
+# summary: Creates, opens, and returns a connection object
+def setItUp():
+    instance = connection.Connection()
+    instance.open_con()
+    return instance
 
-    def openConnection(self):
-        self.__myDB = mysql.connector.connect(
-            host = self.__host,
-            user = self.__user,
-            password = self.__pwd,
-            database = self.__db
-            )
-    
-    def closeConnection(self):
-        self.__myDB.close()
-        self.__myDB = None
-    
-
-
-
-# Connect to MySQL
-connection = Connection("localhost", "root", "", "")
-connection.openConnection()
-
-# Create user database
-cursor = connection.__myDB.cursor()
-cursor.execute("CREATE DATABASE IF NOT EXISTS usersDB;")
-connection.__myDB.database = 'usersDB'
-cursor.execute("USE usersDB;")
-
-# Create table
-cursor.execute("CREATE TABLE IF NOT EXISTS users (username VARCHAR(255) PRIMARY KEY, keyData TEXT);")
-
-# Function to determine if specified user exists and adds data accordingly
+# name: logUser
+# parameters: String username, String keyData
+# summary: Accepts a username and an entry of the passphrase. If the user
+#          is found in the database, updateUser is run. Otherwise, the
+#          user is new and addUser is run.
 def logUser(username, keyData):
-    cursor.execute("SELECT * FROM users WHERE username = %s;", (username,))
+    instance = setItUp()
+    cursor = instance.__mydb.cursor
+    cursor.execute("SELECT * FROM user_data WHERE username = %s;", (username,))
     result = cursor.fetchone()
-    # If the username exists in the table, run addUser
+    instance.close_con()
     if result is not None:
         updateUser(username, keyData)
-    # Otherwise, the user needs to be added to the table
     else:
         addUser(username, keyData)
 
-# Function to add a new user to the table
-def addUser(username, keyData):
-    cursor.execute("INSERT INTO users (username, keyData) VALUES (%s, %s);", (username, keyData))
-    connection.__myDB.commit()
 
-# Function to update the keyData of an existing user
+# name: updateUser
+# parameters: String username, String keyData
+# summary: Accepts a username and an entry of the passphrase. Adds the
+#          passphrase entry into the user's keyData file.
 def updateUser(username, keyData):
-    cursor.execute("UPDATE users SET keyData=%s WHERE username=%s;", (keyData, username))
-    connection.__myDB.commit()
+    instance = setItUp()
+    cursor = instance.__mydb.cursor
+    cursor.execute("INSERT INTO user_data (username, passphrase) VALUES (%s, %s);", (username, keyData))
+    instance.__mydb.commit()
+    instance.close_con()
 
-# Function to return the list of charStateLists of the specified user as string
-def getCompleteData(username):
-    cursor.execute("SELECT keyData FROM users WHERE username = %s;", (username, ))
+# name: addUser
+# parameters: String username, String keyData
+# summary: Accepts a username and an entry of the passphrase. Adds the
+#          user to the database along with the provided entry/entries
+def addUser(username, keyData):
+    instance = setItUp()
+    cursor = instance.__mydb.cursor
+    cursor.execute("UPDATE user_data SET passphrase=%s WHERE username=%s;", (keyData, username))
+    instance.__mydb.commit()
+    instance.close_con()
+
+# name: getData
+# parameters: String username
+# summary: Returns the string representation of the list of CharStateLists for
+#          the specified user
+def getData(username):
+    instance = setItUp()
+    cursor = instance.__mydb.cursor
+    cursor.execute("SELECT passphrase FROM user_data WHERE username = %s;", (username,))
     result = cursor.fetchone()
+    instance.close_con()
     if result is not None:
         return result[0]
     else:
         return None
-
-# Funtion to return the specific charStateList of the specified user at
-#   the specified index as string
-
-
-# Closes connection
-def shutDownDB():
-    cursor.close()
-    connection.closeConnection()
